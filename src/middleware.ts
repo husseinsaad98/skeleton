42ca6ad83e1d1f8831d5e446c15acd342d639d82
Hussein Saad <hussein.saad@Code-technologies.com>
2024-03-13 10:05:38 +0200

    creating login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import db from "./db";
 
 export async function middleware(request: NextRequest) {
-  //   console.log(`[middleware] ${request.method} ${request.url}`);
-  //   const isLoggedIn = await db.isAuthenticated(request.cookies as any);
-  //   if (
-  //     request.nextUrl.pathname &&
-  //     request.nextUrl.pathname.startsWith("/auth")
-  //   ) {
-  //     if (isLoggedIn) {
-  //       return NextResponse.redirect(new URL("/", request.url));
-  //     }
-  //     return;
-  //   }
+  console.log(`[middleware] ${request.method} ${request.url}`);
+  const isLoggedIn = await db.isAuthenticated(request.cookies as any);
+  if (
+    request.nextUrl.pathname &&
+    request.nextUrl.pathname.startsWith("/login")
+  ) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/admin", request.url));
+    }
+    return;
+  }
 
-  //   if (!isLoggedIn) {
-  //     return NextResponse.redirect(new URL(request.url));
-  //     console.log(request.url);
-  //     return NextResponse.redirect(new URL("/auth/login", request.url));
-  //   }
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return NextResponse.next();
 }
